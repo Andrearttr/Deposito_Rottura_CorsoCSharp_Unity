a692e158061c3089797139c7c3eb892f720bc194
Andrearttr <andrearottura03@gmail.com>
2025-06-16 17:56:28 +0200

    Aggiunto esercizio gruppo (da finire)

---
+++ Assets/Scripts/EsercizioGruppoStudenti/ManagerStudenti.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerStudenti : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField _nomeInputField;
+    [SerializeField] private TMP_InputField _cognomeInputField;
+    [SerializeField] private TMP_InputField _etaInputField;
+    [SerializeField] private Dropdown _classeDropdown;
+    [SerializeField] private Slider _votoSlider;
+    [SerializeField] private Button _aggiungiButton;
+    
+}
